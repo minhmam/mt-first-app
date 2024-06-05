604594c06e0a51dd7a7a48749fa288004a41a69f
Phạm Trường Minh <minhkma891@gmail.com>
2024-06-05 17:58:35 +0700

    06-05-2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,20 +4,21 @@ import {HeaderComponent} from "./header/header.component";
 import {UserComponent} from "./user/user.component";
 import {DUMMY_USER} from "./dummy-users";
 import {TasksComponent} from "./tasks/tasks.component";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent, NgForOf, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   users = DUMMY_USER;
-  selectUserId = 'u1'
+  selectUserId?: string;
 
   get selectedUser() {
-    return this.users.find(user => user.id === this.selectUserId)!;
+    return this.users.find(user => user.id === this.selectUserId);
   }
 
   onSelect(id: string) {
--- src/app/dummy-users.ts
+++ src/app/dummy-users.ts
@@ -8,5 +8,15 @@ export const DUMMY_USER = [
     id: 'u2',
     name: 'Phung Hoai Thuong',
     avatar: 'user-1.png'
+  },
+  {
+    id: 'u3',
+    name: 'Pham Truong Giang',
+    avatar: 'user-1.png'
+  },
+  {
+    id: 'u4',
+    name: 'Pham Thanh Nam',
+    avatar: 'user-1.png'
   }
 ]
