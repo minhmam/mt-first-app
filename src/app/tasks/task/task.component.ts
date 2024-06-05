eacc8c37e84792281e0af485c44deaa274d4feb0
thuongphung <ct040249@actvn.edu.vn>
2024-06-05 22:52:01 +0700

    (46) | 06-05-2024

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-task',
@@ -8,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
-
+  @Input() task!: {
+    summary: string;
+    dueDate: string;
+    id: string;
+    title: string;
+    userId: string
+  };
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -9,5 +9,39 @@ import {TaskComponent} from "./task/task.component";
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input({required: true}) name!: string;
+  // @Input({required: true}) name!: string;
+  @Input() user!: {
+    name: string;
+    id: string;
+    avatar: string
+  };
+
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary: 'Learn all the basic and advanced features of Angular and how to apply them.',
+      dueDate: '2025-12-31'
+    },
+    {
+      id: 't2',
+      userId: 'u1',
+      title: 'Spring Boot',
+      summary: 'Learn all the basic and advanced features of Angular and how to apply them.',
+      dueDate: '2025-12-31'
+    },
+    {
+      id: 't3',
+      userId: 'u2',
+      title: 'Java core',
+      summary: 'Learn all the basic and advanced features of Angular and how to apply them.',
+      dueDate: '2025-12-31'
+    }
+  ]
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.user.id);
+  }
 }
+
